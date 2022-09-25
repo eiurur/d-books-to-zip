662c9bfcfbe5495ff6eb922aba4f734a69c4797d
eiurur <eiurur789@gmail.com>
2022-09-25 22:20:49 +0900

    リファクタリング

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const modules = [
-  'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
-];
-modules.forEach((url) => {
-  script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = url;
-  document.getElementsByTagName('head')[0].appendChild(script);
-});
+const INTERVAL_MS = 1500;
+
+const loadModules = () => {
+  const modules = [
+    'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+  ];
+  modules.forEach((url) => {
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+  });
+};
 
 const capture = (currentPage) => {
   return new Promise((resolve) => {
@@ -43,6 +47,7 @@ const download = async (captures) => {
   saveAs(zipBlob, zipName);
   window.URL.revokeObjectURL(zipUrl);
 };
+
 const goToNextPage = () => {
   const screen = document.querySelector('.currentScreen');
   screen.click();
@@ -51,22 +56,28 @@ const goToNextPage = () => {
   e.deltaY = 800;
   screen.dispatchEvent(e);
 };
+
 const sleep = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
+const isShownEndDialog = () =>
+  document.querySelector('#endOfBook').offsetParent !== null;
 
 (async () => {
+  loadModules();
+
   const captures = [];
   while (1) {
     const counter = document.querySelector('#pageSliderCounter');
     const [current, tail] = counter.textContent.split('/');
     console.log(current, tail);
+
     captures.push(await capture(current));
     await goToNextPage();
-    await sleep(1500);
+    await sleep(INTERVAL_MS);
+
     if (Number(current) >= Number(tail)) break;
-    const isShownEndDialog =
-      document.querySelector('#endOfBook').offsetParent !== null;
-    if (isShownEndDialog) break;
+    if (isShownEndDialog()) break;
   }
+
   console.log('END', captures);
   await download(captures);
 })();
